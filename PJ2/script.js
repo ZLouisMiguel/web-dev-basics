dbab900b7533d42a22015bb9a88e9bc457625acf
Louis2324 <miguellouis2023@gmail.com>
2025-11-10 15:14:59 +0200

    Added basic click interactivity

--- PJ2/script.js
+++ PJ2/script.js
@@ -1,5 +1,11 @@
 const btn = document.getElementById("pjbtn");
 
 btn.addEventListener("click", ()=> {
-    alert("I was too lazy to add a project section maybe next time");
+    alert("I was too lazy to add a project section, check my github instead");
 });
+
+const btn2 = document.getElementById("talk");
+
+btn.addEventListener("click", ()=> {
+    alert("Refer to my contact links to talk to me");
+})
