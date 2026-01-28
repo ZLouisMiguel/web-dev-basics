d4806c8acadbf49086a46f2d7ef75452301eea60
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-01-28 08:40:24 +0200

    Card animation using the observer

--- PJ7/src/main.js
+++ PJ7/src/main.js
@@ -35,4 +35,25 @@ function renderProjects(projects) {
   projectGrid.appendChild(fragment);
 }
 
+
+function animateCardsOnScroll() {
+  const cards = document.querySelectorAll(".project-card");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        }
+      });
+    },
+    {
+      threshold: 0.2, 
+    }
+  );
+
+  cards.forEach((card) => observer.observe(card));
+}
+
 renderProjects(projects);
+animateCardsOnScroll();
